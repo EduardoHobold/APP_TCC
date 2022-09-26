a8c4ca0cb66736be4dff89a60b40f28be75d54fb
Eduardo Hobold <hobold.edu@hotmail.com>
2022-09-25 22:58:29 -0300

    Teste

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>TCC HEHE =)</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +14,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#085085',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color: "#FFF",
+    fontWeight: "bold"
+  }
 });
